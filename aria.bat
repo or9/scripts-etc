b260a14d4a28a5ce06147ebef4110744fd7818bb
or9 desktop <durmon.albasir@gmail.com>
2014-04-05 15:29:59 -0400

    more clearly define flow and conditions

--- aria.bat
+++ aria.bat
@@ -4,32 +4,38 @@
 set extension=%~x1
 echo extension is %extension%
 
+goto CHECKARGS
+
+
 rem ###################
 rem # check arguments #
 rem ###################
-:checkargs
-if "%extension%" == ".txt" goto downloadFromListFile
+:CHECKARGS
+if "%extension%" == ".txt" goto DLTXT
+
+rem else
+goto DLURL
 
 rem ###################################
 rem # download single file from a URL #
 rem ###################################
-:downloadFromURL
+:DLURL
 echo downloading single file from URL
-aria2c --on-download-complete=exit --check-integrity=true --file-allocation=falloc --max-concurrent-downloads=5 --continue=true --max-connection-per-server=8 --bt-enable-lpd=true --bt-max-peers=0 "%1"
+rem aria2c --on-download-complete=exit --check-integrity=true --file-allocation=falloc --max-concurrent-downloads=5 --continue=true --max-connection-per-server=8 --bt-enable-lpd=true --bt-max-peers=0 "%1"
 
 goto END
 
 rem #####################################
 rem # download n file(s) from .txt file #
 rem #####################################
-:downloadFromListFile
+:DLTXT
 echo downloading file(s) from .txt doc
 
-aria2c --on-download-complete=exit --check-integrity=true --file-allocation=falloc --max-concurrent-downloads=5 --continue=true --max-connection-per-server=8 --bt-enable-lpd=true --bt-max-peers=0 -i%1
+rem aria2c --on-download-complete=exit --check-integrity=true --file-allocation=falloc --max-concurrent-downloads=5 --continue=true --max-connection-per-server=8 --bt-enable-lpd=true --bt-max-peers=0 -i%1
 
 goto END
 
-if errorlevel gt 0 goto error
+if errorlevel gt 0 goto ERROR
 
 :ERROR
 echo an error occured somewhere
