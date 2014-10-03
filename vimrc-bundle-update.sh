17d508337963356a7eb2e98013fe85fb694dc27e
Durmon AbdulBasir <abdurrahman.abdulbasir@compuware.com>
2014-10-03 11:09:09 -0400

    updated documentation in pathogen updater

--- vimrc-bundle-update.sh
+++ vimrc-bundle-update.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# Example filesystem:
+# ~/.vimrc/bundle/YouCompleteMe
+#	…nerdtree
+#	…molokai
+#	…clang_complete
+#	…vim-airline
+# 
+# Usage (assuming this file's name is update.sh)
+# cd ~/.vimrc/bundle
+# ./update.sh
+
 find . -type d -name .git \
 	| xargs -n 1 dirname \
 	| sort \
