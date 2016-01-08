c74574279bf36938aea500203a316e3cc14f0bf2
drahman <durmon.albasir@gmail.com>
2016-01-07 22:18:32 -0500

    add script to set address

--- macaddress_generator.sh
+++ macaddress_generator.sh
@@ -11,8 +11,20 @@ function default {
 	exit $?
 }
 
+function setaddress {
+	NEW_ADDRESS=`openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//'`
+
+	echo "setting macaddress to $NEW_ADDRESS"
+
+	sudo ifconfig en0 ether $NEW_ADDRESS
+
+	exit $?
+}
+
 if [ -z "$1" ]; then
 	default
+else
+	$1 $#
 fi
 
 exit $?
