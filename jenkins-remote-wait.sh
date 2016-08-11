0f8f8949a11b9d32e625ef5bf905c2092d4ea0a2
drahman <durmon.albasir@gmail.com>
2016-08-11 06:24:57 -0400

    update command

--- jenkins-remote-wait.sh
+++ jenkins-remote-wait.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# run some command & 2>/dev/null &
+#java -jar jenkins-cli.jar -s <url of Jenkins instance> build <project> -s
+#java -jar jenkins-cli.jar -s <url of Jenkins instance> build <project> -f
+# should return 0 or non 0 for success / fail
 pid=$!
 
 spin=('-' '\\' '|' '/')
