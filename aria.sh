2871bce0348b50430fc4b500164d177118dda098
Abdur Rahman <durmon.albasir@gmail.com>
2017-11-23 17:46:02 -0500

    rm deprecated files

--- aria.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#echo "1 is... $1 the file to download"
-aria2c --on-download-complete=exit --check-integrity=true --file-allocation=none --max-concurrent-downloads=8 --continue=true --max-connection-per-server=8 --bt-enable-lpd=true --bt-max-peers=0 $1
-
-exit $?
